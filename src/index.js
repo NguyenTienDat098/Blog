928824c1d9b5b7345b372c3df0e372cce90552d1
NguyenTienDat098 <datga2442004@gmail.com>
2022-07-15 21:53:23 +0700

    push

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const path = require('path');
 const port = 3000;
 const routes = require('./routes');
 
-app.use(
+		app.use(
     express.urlencoded({
         extend: true,
     }),
 );
-app.use(express.json());
+		app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
